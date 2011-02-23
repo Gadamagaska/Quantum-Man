67cf434d2912dddb5f1f91c87759225b3dbd912e
BScient <klblaedel@gmail.com>
2011-02-23 12:35:42 +0100

    Cleaned up Main Fixed tile drawing Fixed where BG is

--- src/core/Direction.java
+++ src/core/Direction.java
@@ -10,7 +10,7 @@ import java.util.Collections;
  */
 public enum Direction {
 	WEST {
-	
+
 	},
 	EAST {
 
--- src/core/FoffyMain.java
+++ src/core/FoffyMain.java
@@ -36,49 +36,74 @@ public class FoffyMain extends Core implements KeyListener {
 		posX = 5;
 		posY = 5;
 		pos.setLocation(posX, posY);
-
-		player = new Player("playersprite", Direction.WEST, pos);
+		player = new Player("playersprite", Direction.SOUTH, pos);
 		s.addKeyListener(this);
-		bg = new ImageIcon("images"+File.separator+"gui"+File.separator+"background.png").getImage();
+	}
+	
+	public synchronized void draw(Graphics2D g) {
+		drawBackground(g);
+		drawBottomTiles(g);
+		drawPlayer(g);
+		drawTopTiles(g);
+		
+		g.drawString("Player Coords: "+player.getX()+","+player.getY(), 200, 25+s.getInsets().top);
+		g.drawString("Ghost Coords: "+player.getGhostX()+","+player.getGhostY(), 500, 25+s.getInsets().top);
+		g.drawString("Map size (h*w): "+Database.getInstance().getLevelDimensions(0).height+","+Database.getInstance().getLevelDimensions(0).width, 300, 590+s.getInsets().top);
 	}
 
-	private void drawTiles(Graphics2D g) {
+	private void drawBottomTiles(Graphics2D g) {
 		int x = 0, y = 0;
 		for(int i = player.getGhostX()-4 ; i < player.getGhostX()+6; i++) {
 			x = 0;
 			for(int j = player.getGhostY()-4 ; j < player.getGhostY()+6 ; j++) {
 				int tile1 = Database.getInstance().getTile(0, 0, i, j);
 				int tile2 = Database.getInstance().getTile(0, 1, i, j);
-				int tile3 = Database.getInstance().getTile(0, 2, i, j);
-				g.drawImage(Database.getInstance().getTileImage("tileset1", tile1), y*50+25, x*50+50, null);
-				g.drawImage(Database.getInstance().getTileImage("tileset1", tile2), y*50+25, x*50+50, null);
-				g.drawImage(Database.getInstance().getTileImage("tileset1", tile3), y*50+25, x*50+50, null);
+				g.drawImage(Database.getInstance().getTileImage("tileset1", tile1), y*50+25, x*50+50+s.getInsets().top, null);
+				g.drawImage(Database.getInstance().getTileImage("tileset1", tile2), y*50+25, x*50+50+s.getInsets().top, null);
 				x += 1;
 			}
 			y += 1;
 		}
 	}
+	
+	private void drawTopTiles(Graphics2D g) {
+		int x = 0, y = 0;
+		for(int i = player.getGhostX()-4 ; i < player.getGhostX()+6 ; i++) {
+			x = 0;
+			for(int j = player.getGhostY()-4 ; j < player.getGhostY()+6 ; j++) {
+				int tile = Database.getInstance().getTile(0, 2, i, j);
+				g.drawImage(Database.getInstance().getTileImage("tileset1", tile), y*50+25, x*50+50+s.getInsets().top, null);
+				x += 1;
+			}
+			y += 1;
+		}
+	}
+	
+	private void drawBackground(Graphics2D g) {
+		bg = new ImageIcon("images"+File.separator+"gui"+File.separator+"background.png").getImage();
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, s.getWidth(), s.getHeight());
+		g.drawImage(bg, 0, s.getInsets().top, null);
+	}
+	
+	private void drawPlayer(Graphics2D g) {
+		Image picture = player.getAnimation().getImage();
+		int x = 25+(player.getX()-player.getGhostX()+4)*50;
+		int y = 50+(player.getY()-player.getGhostY()+4)*50;
+		g.drawImage(picture, x, y+22, null);
+	}
 
+	/**
+	 * Updates the screen
+	 * @param timePassed Time since last update. 
+	 */
 	public void update(long timePassed) {
 		player.update(timePassed);
 	}
 
-	public synchronized void draw(Graphics2D g) {
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, s.getWidth(), s.getHeight());
-		drawTiles(g);
-		g.drawImage(bg, 0, 0, null);
-		// Drawing the player
-		Image picture = player.getAnimation().getImage();
-		int x = 25+(player.getX()-player.getGhostX()+4)*50;
-		int y = 50+(player.getY()-player.getGhostY()+4)*50;
-		g.drawImage(picture, x, y, null);
-		g.drawString("Player Coords: "+player.getX()+","+player.getY(), 200, 25);
-		g.drawString("Ghost Coords: "+player.getGhostX()+","+player.getGhostY(), 500, 25);
-		g.drawString("Map size (h*w): "+Database.getInstance().getLevelDimensions(0).height+","+Database.getInstance().getLevelDimensions(0).width, 300, 590);
-		//g.drawString("FPS: "+fps, 50, 25);
-	}
-	// Keylistener interface
+	/**
+	 * Key events
+	 */
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
 			stop();
