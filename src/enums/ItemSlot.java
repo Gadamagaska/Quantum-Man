529ceda54e706a0806a37e277fd5b501cccd6677
Emil <Emil@192.168.1.102>
2011-02-26 17:16:19 +0100

    JavaDoc for ItemSlot

--- src/enums/ItemSlot.java
+++ src/enums/ItemSlot.java
@@ -1,5 +1,11 @@
 package enums;
 
+/**
+ * This enum defines the different types of ItemSlot's a player has
+ * in the game.
+ * @author Emil
+ *
+ */
 public enum ItemSlot {
 	WEAPON, ARMOR, UNEQUIPPABLE
 }
